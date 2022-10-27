42f60ee3838b6dd9340a69a3b83e6ad827eb21a8
opuzayed <zayedopu4933@gmail.com>
2022-10-27 12:14:40 +0600

    faq section done

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,81 @@
+import React, { useContext } from 'react';
+import { Image } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import LeftSideNav from '../LeftSideNav/LeftSideNav';
+import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
+    return (
+        <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
+            <Container>
+            <img
+              src="./../../../assets/brands/logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
+                <Navbar.Brand> <Link to='/'>GigaTech</Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        {/* <Nav.Link href="/">Courses</Nav.Link>
+                        <Nav.Link href="#pricing">FAQ</Nav.Link>
+                        <Nav.Link href="#pricing">Blog</Nav.Link> */}
+                       <>
+                       <Link to="/">Courses</Link>
+                        <Link to="/questions">FAQ</Link>
+                        <Link to="/">Blog</Link>
+                       </>
+                    </Nav>
+                    <Nav>
+                        <>
+                            {
+                                user?.uid ?
+                                    <>
+                                        <span>{user?.displayName}</span>
+                                        <Button variant="light" onClick={handleLogOut}>Log out</Button>
+                                    </>
+                                    :
+                                    <>
+                                        <Link to='/login'>Login</Link>
+                                        <Link to='/register'>Register</Link>
+                                    </>
+                            }
+
+
+                        </>
+                        <Link to="/profile">
+                            {user?.photoURL ?
+                                <Image
+                                    style={{ height: '30px' }}
+                                    roundedCircle
+                                    src={user?.photoURL}>
+                                </Image>
+                                : <FaUser></FaUser>
+                            }
+                        </Link>
+                    </Nav>
+                    <div className='d-lg-none'>
+                        <LeftSideNav></LeftSideNav>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
