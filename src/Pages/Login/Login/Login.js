8bcc89c7ce5751e2a0a92b139a5c8f956b274e04
opuzayed <zayedopu4933@gmail.com>
2022-10-28 16:53:04 +0600

    login done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
         signIn(email, password)
             .then(result => {
                 const user = result.user;
-                // console.log(user);
+                console.log(user);
                 form.reset();
                 setError('');
                 if(user.emailVerified){
